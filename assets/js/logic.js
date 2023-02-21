9b17943f5b22ec852b1050fcaf3a6f83fb3135ac
Gatsby <115715220+onderguler35@users.noreply.github.com>
2023-02-21 03:26:47 +0000

    questions and answers working now, working on the scoeboard

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,10 +2,15 @@ var questionIndex = 0;
 var questionsDiv = document.getElementById("questions");
 var startDiv = document.getElementById("start-screen");
 var questionTitle = document.getElementById("question-title");
-var time = 600;
+var time = 60;
 const incorrect = new Audio("assets/sfx/incorrect.wav");
 const correct = new Audio("assets/sfx/correct.wav");
 var timeDisplay = document.getElementById("time");
+var choicesDiv = document.getElementById("choices");
+var button;
+var options;
+var correctAnswer;
+var feedback = document.getElementById("feedback");
 
 function countDown() {
   time--;
@@ -24,16 +29,11 @@ function startQuiz() {
   questions.sort(() => Math.random() - 0.5);
   showQuestions();
 }
-var choicesDiv = document.getElementById("choices");
-var button; 
-var options;
-var correctAnswer;
-var feedback = document.getElementById("feedback");
 
 function showQuestions() {
-  options = '';
+  options = "";
   options = questions[questionIndex].choices;
-  questionTitle.innerHTML = '';
+  questionTitle.innerHTML = "";
   questionTitle.innerHTML = questions[questionIndex].title;
   choicesDiv.textContent = "";
   for (var i = 0; i < options.length; i++) {
@@ -42,33 +42,31 @@ function showQuestions() {
     choicesDiv.append(button);
     choicesDiv.addEventListener("click", checkAnswer);
   }
-  console.log(options, choicesDiv);
+}
 
 function checkAnswer(event) {
-  correctAnswer = '';
+  correctAnswer = "";
   correctAnswer = questions[questionIndex].answer;
-  console.log(correctAnswer)
+  console.log(correctAnswer);
   if (correctAnswer != event.target.innerHTML) {
-    time = time - 2;
+    time = time - 10;
     incorrect.play();
     feedback.textContent = "Incorrect!";
-  } 
-  else {
+  } else {
     correct.play();
     feedback.textContent = "Correct!";
   }
   feedback.removeAttribute("class", "hide");
-    setTimeout(() => {
-      feedback.setAttribute("class", "hide");
-      feedback.textContent = "";
-    }, 1000);
-    console.log(questionIndex, event.target.innerText);
-    if (questionIndex >= questions.length) endQuiz();
-    questionIndex++;
-    showQuestions();
-}  
-  
+  setTimeout(() => {
+    feedback.setAttribute("class", "hide");
+    feedback.textContent = "";
+  }, 1000);
+
+  questionIndex++;
+  if (questionIndex >= questions.length) endQuiz();
+  showQuestions();
 }
+
 var finalScore = document.getElementById("final-score");
 var resultDiv = document.getElementById("end-screen");
 
@@ -76,5 +74,15 @@ function endQuiz() {
   clearInterval(counter);
   resultDiv.removeAttribute("class", "hide");
   questionsDiv.setAttribute("class", "hide");
+  timeDisplay.innerHTML = time;
   finalScore.textContent = time;
 }
+
+function submit(time) {
+  inputInitials = document.getElementById("initials");
+  playerInitials = inputInitials.innerHTML;
+  
+};
+localStorage.setItem('scoreMemory', JSON.stringify(score:{time}, Initials:{playerInitials}));
+console.log(inputInitials, playerInitials, obj);
+
