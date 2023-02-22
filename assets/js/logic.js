ae7ce2cccddcd4d667041852cdd0b35c3cbd61e1
Gatsby <115715220+onderguler35@users.noreply.github.com>
2023-02-22 05:01:48 +0000

    commented the code in logic js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,102 +1,97 @@
-var questionIndex = 0;
-var questionsDiv = document.getElementById("questions");
-var startDiv = document.getElementById("start-screen");
-var questionTitle = document.getElementById("question-title");
-var time = 60;
-const incorrect = new Audio("assets/sfx/incorrect.wav");
-const correct = new Audio("assets/sfx/correct.wav");
+// Let's grab the elements we need and assign them to variables ready for  use when we need them
 var timeDisplay = document.getElementById("time");
+var startDiv = document.getElementById("start-screen");
+var questionsDiv = document.getElementById("questions");
+var questionTitle = document.getElementById("question-title");
 var choicesDiv = document.getElementById("choices");
+var feedback = document.getElementById("feedback");
+var finalScore = document.getElementById("final-score");
+var resultDiv = document.getElementById("end-screen");
+var submitBtn = document.getElementById('submit');
+var initials = document.getElementById('initials')
 var button;
 var options;
 var correctAnswer;
-var feedback = document.getElementById("feedback");
+// the audio files are assigned to variables to play when needed
+const correct = new Audio("assets/sfx/correct.wav");
+const incorrect = new Audio("assets/sfx/incorrect.wav");
+var time = 60; // Quiz time is set to 60 secs when time is up quiz ends
+var questionIndex = 0; //our index to determine which question currently we are dealing with
 
-
+// countdown function counts down from 60 secs to limit the quiz time. it ends the quiz after clearing interval
 function countDown() {
   time--;
   timeDisplay.innerHTML = time;
-  if (time < 1) {
-    time = 0;
+  if (time < 1) {  //we don't want the counter to negative values
     clearInterval(counter);
-    endQuiz();
+    endQuiz(); // time is up. drop the mouse!
   }
 }
 
+// startQuiz function hides starting screen, starts countdown function and shuffles questions
 function startQuiz() {
-  startDiv.setAttribute("class", "hide");
-  counter = setInterval(countDown, 1000);
-  questionsDiv.removeAttribute("class", "hide");
-  questions.sort(() => Math.random() - 0.5);
+  startDiv.setAttribute("class", "hide"); //hide the initial screen 
+  counter = setInterval(countDown, 1000); //repeats the countdown function every sec until cleared
+  questionsDiv.removeAttribute("class", "hide"); // make questions visible
+  questions.sort(() => Math.random() - 0.5); // this code shuffles the questions array so each time the quiz starts it changes the order of questions randomly. Less boring ! :)
   showQuestions();
 }
 
 function showQuestions() {
-  options = "";
-  options = questions[questionIndex].choices;
-  questionTitle.innerHTML = "";
-  questionTitle.innerHTML = questions[questionIndex].title;
-  choicesDiv.textContent = "";
+  options = questions[questionIndex].choices; // assigns the choices part of question array for each question 
+  questionTitle.innerHTML = questions[questionIndex].title; // assings each question grabing it from the title seciton of the questions array which is the question itself
+  choicesDiv.textContent = ""; // let's clear the area so it won't show previous questions options
+  // below for loop iterates the options for each answer and creates a button for each option and appends it to choicesDiv so the necessary html is created dynamically.
   for (var i = 0; i < options.length; i++) {
-    button = document.createElement("button");
+    button = document.createElement("button"); //creates a button for each option
     button.innerHTML = options[i];
-    choicesDiv.append(button);
-    choicesDiv.addEventListener("click", checkAnswer);
+    choicesDiv.append(button); //appends it to choicesDiv
+    choicesDiv.addEventListener("click", checkAnswer); // listening for a click to one of the choices of the answer and calls the checkAnswer function to see if it is correct or incorrect
   }
 }
 
 function checkAnswer(event) {
-  correctAnswer = "";
-  correctAnswer = questions[questionIndex].answer;
-  console.log(correctAnswer);
+  correctAnswer = questions[questionIndex].answer; //getting each correct answer from the questions array depending on the value of the questionIndex which determines the current question
   if (correctAnswer != event.target.innerHTML) {
-    time = time - 10;
-    incorrect.play();
-    feedback.textContent = "Incorrect!";
+    time = time - 10; // penalise the player by 10 secs deducted from the score time after a wrong answer.
+    incorrect.play(); // plays the sound for wrong answers
+    feedback.textContent = "Incorrect!"; // modifying the feedback to confirm wrong answer
   } else {
-    correct.play();
-    feedback.textContent = "Correct!";
+    correct.play(); // plays the sound for correct answers
+    feedback.textContent = "Correct!";// modifying the feedback to confirm correct answer
   }
-  feedback.removeAttribute("class", "hide");
+  feedback.removeAttribute("class", "hide"); // makes feedback line visible  and the below setTime out clears it after 1 sec.
   setTimeout(() => {
     feedback.setAttribute("class", "hide");
-    feedback.textContent = "";
+    
   }, 1000);
-
-  questionIndex++;
-  if (questionIndex >= questions.length) endQuiz();
-  showQuestions();
+  questionIndex++; // here is where we increment the questionIndex to follow the next question
+  if (questionIndex >= questions.length) endQuiz(); // if done with all questions, runs the endQuiz function
+  showQuestions(); // if not done with all questions repeat the showQuestions process until all done
 }
-
-var finalScore = document.getElementById("final-score");
-var resultDiv = document.getElementById("end-screen");
 
 function endQuiz() {
-  clearInterval(counter);
-  resultDiv.removeAttribute("class", "hide");
-  questionsDiv.setAttribute("class", "hide");
+  clearInterval(counter); // stop the clock ticking, time is up!
+  questionsDiv.setAttribute("class", "hide"); // hides the questions screen
+  resultDiv.removeAttribute("class", "hide"); // makes the result screen visible
   if (time<0) time = 0; //if score goes negative value, this resets it to make sure it will be 0 minimum
-  timeDisplay.innerHTML = time;
-  finalScore.textContent = time;
-  submitBtn.addEventListener('click',submit);
+  timeDisplay.innerHTML = time; // shows the end time to time display
+  finalScore.textContent = time;// the remaining secs are used as score as per the requirements
+  submitBtn.addEventListener('click',submit); // listening to the submit button which will take the initials and score and put to the local storage with the submit function
 }
-var submitBtn = document.getElementById('submit');
-var initials = document.getElementById('initials')
-function submit() {
-  var newScore = {
+function submit() {//let's create a new object with the initials entered and the score done.
+  var newScore = { 
     initials : initials.value,
     score : time
   };
-  var scoreArr = [];
-  // checking if the local storage is empty to avoid null and add first score in
-  if (localStorage.getItem('scoreBoard') == null) {
-    scoreArr.push(newScore);
+  var scoreArr = []; // assings variable of the array which we will gather all the names and scores in the scoreboard in the memory. it is empty yet.
+  if (localStorage.getItem('scoreBoard') == null) {         // checking if the local storage is empty to avoid null and add first score in and then
+    scoreArr.push(newScore);   // pushes our new object into the array of the scores no need to retrieve the data from the localstorage first as it is empty
   }
   else {
-    scoreArr = JSON.parse(localStorage.getItem('scoreBoard'));
-    scoreArr.push(newScore);    
+    scoreArr = JSON.parse(localStorage.getItem('scoreBoard'));      // if there is already stored data in local storage, we retrieve it first to add our obj into it.
+    scoreArr.push(newScore);      
   }
-
-  localStorage.setItem('scoreBoard', JSON.stringify(scoreArr));
-  window.open('./highscores.html'); 
+  localStorage.setItem('scoreBoard', JSON.stringify(scoreArr)); // now our combined score array is ready to be sent back to localstorge in one peice after converting into json string as that is the only way local storage accepts data
+  window.open('./highscores.html'); // after initials entered and submitted this takes us to highscores html page to show the list of scores everbody else scored
 };
